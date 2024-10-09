0bc94f69c7e312ca19bc28432e4ff9cfe34ff7fa
David Del Sol <daviddelsol1998@gmail.com>
2024-10-09 11:49:40 -0500

    fixes to component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { Contact } from './contacts/contacts.component';
-import { ContactListComponent } from './contacts/contact-list/contact-list.component';
-import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
-import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    Contact,
-    ContactListComponent,
-    ContactDetailComponent,
-    ContactItemComponent
+    ContactsComponent // Remove Contact from here if it's not a component
   ],
   imports: [
     BrowserModule,
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -5,31 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
+export class ContactsComponent implements OnInit {
+  id: string;
 
-export class Contact implements OnInit{
-  public id: string;
-  public name: string;
-  public email: string;
-  public phone: string;
-  public imageUrl: string;
-  public group?: Contact[];
-  
-  constructor(
-  id: string,
-  name: string,
-  email: string,
-  phone: string,
-  imageUrl: string,
-  group?: Contact[]
-  )
-  {
-  this.id = id;
-  this.name = name;
-  this.email = email;
-  this.phone = phone;
-  this.imageUrl = imageUrl;
-  this.group = group;
+  constructor() {
+    this.id = ''; // Initialize id in the constructor
   }
-  ngOnInit(): void {
+
+  ngOnInit() {
+    // Initialization logic here
   }
 }
