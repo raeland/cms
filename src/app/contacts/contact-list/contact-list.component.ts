9287ceacec73f1b3c8d039308f58a77fe13c398b
raeland <rsvarady@gmail.com>
2024-10-15 22:57:46 -0400

    directions4usingEventEmitters2.b.i is broken :(

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -11,14 +11,14 @@ import { ContactItemComponent } from '../contact-item/contact-item.component';
   styleUrls: ['./contact-list.component.css'],
 })
 export class ContactListComponent {
-  @Output() contactSelected = new EventEmitter<Contact>();
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
   contacts: Contact[] = [
     new Contact("1", "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "assets/images/jacksonk.jpg", []),
     new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "assets/images/barzeer.jpg", [])
   ];
 
-  onContactSelected(contact: Contact) {
-    this.contactSelected.emit(contact);
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
   }
 
   constructor() { }
