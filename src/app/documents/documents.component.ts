2e15fb433aca77a6b65e7c92da154d69d438717a
raeland <rsvarady@gmail.com>
2024-10-17 22:05:30 -0400

    W04-docComponent

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Document } from './document.model';
+
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
-  styleUrl: './documents.component.css'
+  styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent {
-
+  selectedDocument: Document;
+  
 }
