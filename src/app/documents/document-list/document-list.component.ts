adc2f38797b7febf4c1a4df187f571c46958efee
raeland <rsvarady@gmail.com>
2024-10-17 21:54:21 -0400

    W04-docList-done

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter();
 
+  documents: Document[] = [
+    new Document('1', 'Doc1', 'DocDescription1', 'www.abc1.org', []),
+    new Document('2', 'Doc2', 'DocDescription2', 'www.abc2.org', []),
+    new Document('3', 'Doc3', 'DocDescription3', 'www.abc3.org', []),
+    new Document('4', 'Doc4', 'DocDescription4', 'www.abc4.org', []),
+    new Document('5', 'Doc5', 'DocDescription5', 'www.abc5.org', [])
+  ];
+  
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
