0cf5736879d85cfe0eaf078e9846edf3a87b7d88
raeland <rsvarady@gmail.com>
2024-10-28 21:49:29 -0400

    W05-implement-docService

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'app-document-list',
@@ -11,13 +12,19 @@ export class DocumentListComponent {
   @Output() selectedDocumentEvent = new EventEmitter();
 
   documents: Document[] = [
-    new Document('1', 'Doc1', 'DocDescription1', 'www.abc1.org', []),
-    new Document('2', 'Doc2', 'DocDescription2', 'www.abc2.org', []),
-    new Document('3', 'Doc3', 'DocDescription3', 'www.abc3.org', []),
-    new Document('4', 'Doc4', 'DocDescription4', 'www.abc4.org', []),
-    new Document('5', 'Doc5', 'DocDescription5', 'www.abc5.org', [])
+    // new Document('1', 'Doc1', 'DocDescription1', 'www.abc1.org', []),
+    // new Document('2', 'Doc2', 'DocDescription2', 'www.abc2.org', []),
+    // new Document('3', 'Doc3', 'DocDescription3', 'www.abc3.org', []),
+    // new Document('4', 'Doc4', 'DocDescription4', 'www.abc4.org', []),
+    // new Document('5', 'Doc5', 'DocDescription5', 'www.abc5.org', [])
   ];
-  
+selectedDocument: Document;
+
+constructor(private documentService: DocumentService) {}
+
+  ngOnInit(): void {
+    this.documents = this.documentService.getDocuments();
+  }
   onSelectedDocument(document: Document) {
     this.selectedDocumentEvent.emit(document);
   }
