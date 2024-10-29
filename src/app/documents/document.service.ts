cefaef5abdfee19d5aea5db5095424bae0eab8b1
raeland <rsvarady@gmail.com>
2024-10-28 21:49:31 -0400

    W05-docService

---
+++ src/app/documents/document.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, EventEmitter } from '@angular/core';
+
+import { Document } from './document.model';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+  private documents: Document[] = MOCKDOCUMENTS;
+
+  documentSelectedEvent = new EventEmitter<Document>();
+
+  constructor() { 
+    this.documents = MOCKDOCUMENTS;
+   }
+   getDocuments(): Document[] {
+    return this.documents.slice();
+  }
+
+  getDocument(id:string): Document | null {
+    for (let document of this.documents) {
+      if (document.id === id) {
+        return document;
+      }
+    }
+    return null;
+  }
+}
