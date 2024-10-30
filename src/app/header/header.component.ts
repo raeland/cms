376dd85fbdcc05983caeceb4b10321e827998491
raeland <rsvarady@gmail.com>
2024-10-29 20:52:04 -0400

    W06-routerLink-directives

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,13 +7,13 @@ import { EventManager } from '@angular/platform-browser';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  @Output() selectedFeatureEvent = new EventEmitter<string>();
+  // @Output() selectedFeatureEvent = new EventEmitter<string>();
 
   // constructor() { }
 
-  onSelected(selectedEvent: string) {
-    this.selectedFeatureEvent.emit(selectedEvent);
-  }
+  // onSelected(selectedEvent: string) {
+  //   this.selectedFeatureEvent.emit(selectedEvent);
+  // }
 
   isUserDropdownOpen: boolean = false;
   isNavbarCollapsed: boolean = true;
