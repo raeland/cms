48783d9a98126ab69f1e27f0ea4ba334f2092599
raeland <rsvarady@gmail.com>
2024-10-29 20:46:26 -0400

    W06-Routed-Displayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   //title = 'cms';
-  selectedFeature = 'contact';
+  // selectedFeature = 'contact';
 
-  switchView(selectedFeature: string) {
-    this.selectedFeature = selectedFeature;
-  }
+  // switchView(selectedFeature: string) {
+  //   this.selectedFeature = selectedFeature;
+  // }
 }
